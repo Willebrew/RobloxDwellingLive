45bff5f0185cd3c8137b0d07568dcbf8e566919e
Will Killebrew <will.killebrew@du.edu>
2024-11-27 22:06:54 -0600

    Bug fix

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ admin.initializeApp({
         projectId: process.env.FIREBASE_PROJECT_ID,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        API_KEY: process.env.API_KEY
     }),
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
